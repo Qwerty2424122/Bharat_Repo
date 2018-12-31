5501970f5fc72a8a9330934b68dfd934389b7c97
Bharat Bhushan Sharma <34498660+Qwerty2424122@users.noreply.github.com>
2018-12-31 04:23:37 +0000

    Some work on Turtle graphics

---
+++ 4.py
@@ -0,0 +1,21 @@
+import turtle
+import tkinter
+
+win = turtle.Screen()
+win.bgcolor("green")
+#win.screen.title("Welcome to the turtle zoo!")
+new_turtle = turtle.Turtle()
+new_turtle.color("red")
+new_turtle.forward(100)
+new_turtle.right(90)
+
+new_turtle.forward(100)
+new_turtle.right(90)
+
+new_turtle.forward(100)
+new_turtle.right(90)
+
+new_turtle.forward(100)
+new_turtle.right(90)
+#tkinter.mainloop()
+#win.exitonclick()
